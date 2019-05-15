2a4da3eac018897c4de3678d2adcd32817b53694
omar.panebianco <omar.panebianco@172.22.15.85>
2019-05-15 13:51:01 +0200

    Implementata GUI eser1

--- src/it/polito/tdp/borders/BordersController.java
+++ src/it/polito/tdp/borders/BordersController.java
@@ -6,7 +6,9 @@ package it.polito.tdp.borders;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.Set;
 
+import it.polito.tdp.borders.model.Country;
 import it.polito.tdp.borders.model.Model;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -31,8 +33,28 @@ public class BordersController {
 
 	@FXML
 	void doCalcolaConfini(ActionEvent event) {
-
-		txtResult.setText("Todo!");
+		this.txtResult.clear();
+		
+		try {
+			int year = Integer.parseInt(this.txtAnno.getText().trim());
+			
+			if(year < 1816 || year > 2016) {
+				this.txtResult.appendText("Errore: range anno non valido. Inserire un anno compreso tra il 1816 e il 2016 inclusi.");
+				return;
+			}
+			
+			model.createGraph(year);
+			
+			Set<Country> vertexSet = model.getVertexSet();		
+			for(Country c : vertexSet)
+				this.txtResult.appendText(String.format("Stato: %s, Confinanti: %d\n", c.getStateName(), model.findDegree(c)));
+			
+			this.txtResult.appendText("\nComponenti connesse: " + model.getNumberOfConnectedComponents());
+			
+		} catch (NumberFormatException e) {
+			this.txtResult.appendText("Errore: input anno non valido.");
+			e.printStackTrace();
+		}
 	}
 
 	@FXML // This method is called by the FXMLLoader when initialization is complete
@@ -40,4 +62,9 @@ public class BordersController {
 		assert txtAnno != null : "fx:id=\"txtAnno\" was not injected: check your FXML file 'Borders.fxml'.";
 		assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Borders.fxml'.";
 	}
+	
+	public void setModel(Model model) {
+		this.model = model;
+	}
+	
 }
